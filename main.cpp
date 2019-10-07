0c67192179a4dc385a7c01e8775e80872eb77d6c
unifrsilva <renanmartins1999@hotmail.com>
2019-10-07 17:29:46 -0300

    Started implementing CRUD.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 #include "Pilha.h"
 
+std::string menuCrud(){
+   return "\n1 - INSERIR PRODUTO\n2 - REMOVER PRODUTO\n3 - LISTAR PRODUTOS\n4 - SAIR\n";
+};
+
+using namespace std;
 int main() {
-    std::cout << "Controle de Estoque\n" << std::endl;
+    std::cout << "================> Controle de Estoque <==============" << std::endl;
+    int opc;
+
+    while(opc != 4){
+        cout << menuCrud();
+        cout << "Digite uma opcao: ";
+        cin >> opc;
+        if(opc == 1){
+
+        }
+
+        if(opc == 2){
+
+        }
+
+        if(opc == 3){
+
+        }
+    }
 
     return 0;
 }
