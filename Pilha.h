42598f24769179fe86d9246dc88bb38cbdbe1949
unifrsilva <renanmartins1999@hotmail.com>
2019-10-07 16:34:34 -0300

    Fixing some issues.

---
+++ Pilha.h
@@ -0,0 +1,12 @@
+//
+// Created by unifrsilva on 07/10/2019.
+//
+#include <iostream>
+#ifndef STOCKCONTROL_PILHA_H
+#define STOCKCONTROL_PILHA_H
+
+#endif //STOCKCONTROL_PILHA_H
+
+class Pilha{
+
+};
---
+++ Produto.h
@@ -0,0 +1,12 @@
+//
+// Created by unifrsilva on 07/10/2019.
+//
+#include <iostream>
+#ifndef STOCKCONTROL_PRODUTO_H
+#define STOCKCONTROL_PRODUTO_H
+
+#endif //STOCKCONTROL_PRODUTO_H
+
+class Produto{
+
+};
