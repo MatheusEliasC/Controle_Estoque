c4d609acd8274ca04ed926a087cac1bfe8a501f2
unifrsilva <renanmartins1999@hotmail.com>
2019-10-15 19:16:12 -0300

    My commit message.

---
+++ cmake-build-debug/CMakeFiles/stockControl.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stockControl.dir/main.cpp.o"
+  "libstockControl.dll.a"
+  "stockControl.exe"
+  "stockControl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stockControl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
